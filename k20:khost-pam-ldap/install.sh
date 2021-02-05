a03c19b80ac129ed774495e3da4ca626f744a197
gorry <gorry@localhost.localdomain>
2021-02-05 08:49:40 +0100

    kerberos

--- k20:khost-pam-ldap/install.sh
+++ k20:khost-pam-ldap/install.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 # Khost-pam-ldap
 # @edt ASIX M11-SAD Curs 2020-2021
-
-cp /opt/docker/system-auth /etc/pam.d/system-auth 
+ 
 cp /opt/docker/krb5.conf /etc/krb5.conf
+bash /opt/docker/auth.sh
+#cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
+#cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
+#cp /opt/docker/nslcd.conf /etc/nslcd.conf
+#cp /opt/docker/system-auth /etc/pam.d/system-auth
+
+
 #dnf -y install pam_krb5-2.4.8-6.el7.x86_64.rpm
 
 # crear usuaris local01--03 (IP+AP)
--- k20:khost-pam-ldap/startup.sh
+++ k20:khost-pam-ldap/startup.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
 /opt/docker/install.sh && echo "Ok install"
+
+/sbin/nscd && echo "Ok nscd"
+/sbin/nslcd && echo "Ok nslcd"
+
 /bin/bash
